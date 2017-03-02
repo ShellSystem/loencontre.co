3f1455065a194afa4768de29ad6a7ecdba3ab5e9
larry852 <larryportocarrero@gmail.com>
2017-03-02 10:24:52 -0500

    Version corroccion angular

--- Aplication/angular/main.js
+++ Aplication/angular/main.js
@@ -1,79 +1,100 @@
 var app = angular.module('mainModule', []);
 
+app.directive('dynamic', function ($compile) {
+	return {
+		restrict: 'A',
+		replace: true,
+		link: function (scope, ele, attrs) {
+			scope.$watch(attrs.dynamic, function(html) {
+				ele.html(html);
+				$compile(ele.contents())(scope);
+			});
+		}
+	};
+});
+
+
 app.controller('controller',function($scope){
 
+	$scope.view = function(post){
+		$scope.html = '<article><a class="thumbnail" href="images/fulls/'+post.image+'.jpg">'+
+		'<img src="images/thumbs/'+post.image+'.jpg" alt="" /></a>'+
+		'<h2>'+post.date+'</h2>'+
+		'<p><a href="https://www.facebook.com/photo.php?'+post.link+'">Ir al post</a></p></article>';
+	}
+
 	$scope.posts = [
-		{
-			id: '01',
-			link: '1423565230987886',
-			image: '06',
-			date: '16 January 2017'
-		},
-		{
-			id: '02',
-			link: '1423565184321224',
-			image: '07',
-			date: '16 January 2017'
-		},
-		{
-			id: '03',
-			link: '1198260166917436',
-			image: '08',
-			date: '22 December 2016'
-		},
-		{
-			id: '04',
-			link: '624820691036869',
-			image: '09',
-			date: '22 December 2016'
-		},
-		{
-			id: '05',
-			link: '10210186212226924',
-			image: '10',
-			date: '13 December 2016'
-		},
-		{
-			id: '06',
-			link: '1347375305314426',
-			image: '11',
-			date: '13 December 2016'
-		},
-		{
-			id: '07',
-			link: '10209736373119231',
-			image: '13',
-			date: '7 December 2016'
-		},
-		{
-			id: '08',
-			link: '1744526859203295',
-			image: '14',
-			date: '6 December 2016'
-		},
-		{
-			id: '09',
-			link: '1617963344896071',
-			image: '03',
-			date: '21 November 2016'
-		},
-		{
-			id: '10',
-			link: '1185318914885958',
-			image: '05',
-			date: '23 January 2017'
-		},
-		{
-			id: '11',
-			link: '1167876316635725',
-			image: '15',
-			date: '7 December 2016'
-		},
-		{
-			id: '12',
-			link: '10211595648035909',
-			image: '12',
-			date: '7 December 2016'
-		}
+	{
+		id: '01',
+		link: '1423565230987886',
+		image: '06',
+		date: '16 January 2017'
+	},
+	{
+		id: '02',
+		link: '1423565184321224',
+		image: '07',
+		date: '16 January 2017'
+	},
+	{
+		id: '03',
+		link: '1198260166917436',
+		image: '08',
+		date: '22 December 2016'
+	},
+	{
+		id: '04',
+		link: '624820691036869',
+		image: '09',
+		date: '22 December 2016'
+	},
+	{
+		id: '05',
+		link: '10210186212226924',
+		image: '10',
+		date: '13 December 2016'
+	},
+	{
+		id: '06',
+		link: '1347375305314426',
+		image: '11',
+		date: '13 December 2016'
+	},
+	{
+		id: '07',
+		link: '10209736373119231',
+		image: '13',
+		date: '7 December 2016'
+	},
+	{
+		id: '08',
+		link: '1744526859203295',
+		image: '14',
+		date: '6 December 2016'
+	},
+	{
+		id: '09',
+		link: '1617963344896071',
+		image: '03',
+		date: '21 November 2016'
+	},
+	{
+		id: '10',
+		link: '1185318914885958',
+		image: '05',
+		date: '23 January 2017'
+	},
+	{
+		id: '11',
+		link: '1167876316635725',
+		image: '15',
+		date: '7 December 2016'
+	},
+	{
+		id: '12',
+		link: '10211595648035909',
+		image: '12',
+		date: '7 December 2016'
+	}
 	]
 });
