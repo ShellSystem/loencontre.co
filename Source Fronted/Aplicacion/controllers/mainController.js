fb7ea16e7bb57a8ee3c9433a834272c46b8d2b69
larry852 <larryportocarrero@gmail.com>
2017-03-16 07:22:16 -0500

    Ajax y paginacion

--- Source Fronted/Aplicacion/controllers/mainController.js
+++ Source Fronted/Aplicacion/controllers/mainController.js
@@ -1,22 +1,49 @@
 class MainController {
 
   constructor() {
-    var database = new Database();
-    this.posts = database.posts;
+    this.init();
+
   }
   
-  setPosts(){
-    for (var i in this.posts) {
-      var post = this.posts[i];
-      $("#thumbnails").append('<article>' +
-        '<a class="thumbnail" href="images/fulls/'+post.image+'.jpg" data-position="center"><img src="images/thumbs/'+post.image+'.jpg" alt="" /></a>'+
-        '<h2>'+post.date+'</h2>' +
-        '<p><a href="https://www.facebook.com/photo.php?fbid='+post.link+'" target="_blank">Ir al post</a></p>' +
-        '</article>');
-    }
-    
-  }
-  
+  init(){
+    $.ajax({
+          dataType: "json",
+          url: "http://entregascontinuas.goodfirmcolombia.co/get-page?pageNumber=1"
+        }).done(function(data) {
+          setPost(data);
+        });
+
+    $.ajax({
+      dataType: "json",
+      url: "http://entregascontinuas.goodfirmcolombia.co/pagination"
+    }).done(function(pages) {
+      $('#pagination-here').bootpag({
+        total: pages.pageAmount
+      }).on("page", function(event, num){
+        $.ajax({
+          dataType: "json",
+          url: "http://entregascontinuas.goodfirmcolombia.co/get-page?pageNumber="+num
+        }).done(function(data) {
+          setPost(data);
+        });
+      });
+    });
+  }  
 }
-var controller = new MainController();
-controller.setPosts();
+
+function setPost(data) {
+  $("#thumbnails").html('');
+  for (var i in data) {
+    var post = data[i];
+    $("#thumbnails").append('<article>' +
+      '<a class="thumbnail" href="'+post.image+'" data-position="center"><img src="'+post.image+'" alt="" /></a>'+
+      '<h2>'+post.date+'</h2>' +
+      '<p><a href="https://www.facebook.com/photo.php?fbid='+post.link+'" target="_blank">Ir al post</a></p>' +
+      '</article>');
+  }
+}
+
+
+$(document).ready(function() {
+  var controller = new MainController();
+});
