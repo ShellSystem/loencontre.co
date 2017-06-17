7e00f16834e75a4c3347a5e2c17c3a640e8c5fd1
Álvaro Ramiro Hernández Millán <alvarohernandezmillan11@gmail.com>
2017-06-17 17:13:55 +0000

    Corrigiendo en web.php Postcontroller por PostController

--- SourceBackend/laravel/routes/web.php
+++ SourceBackend/laravel/routes/web.php
@@ -20,7 +20,7 @@ Route::get('date-range', 'PostController@dateRange');
 Route::any('add-post', 'PostController@addPost');
 Route::any('search-name', 'PostController@searchName');
 Route::any('user-posts', 'PostController@userPosts');
-Route::any('change-state', 'Postcontroller@changeState');
+Route::any('change-state', 'PostController@changeState');
 
 Route::any('prueba', 'PostController@prueba');
 
