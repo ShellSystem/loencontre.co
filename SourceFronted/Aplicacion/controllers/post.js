06b92f2f305f94cb52bafe5ee380baed25517779
larry852 <larryportocarrero@gmail.com>
2018-08-06 11:18:44 -0500

    No facebok

--- SourceFronted/Aplicacion/controllers/post.js
+++ SourceFronted/Aplicacion/controllers/post.js
@@ -25,6 +25,7 @@ function newPost(txtFilter, user) {
     $("#user_name").val(user.name);
     $("#user_email").val(user.email);
     post = new FormData($("#new")[0]);
+    console.log(post);
     $.ajax({
         type: "POST",
         url: base + "loencontre/SourceBackend/add-post",
@@ -72,7 +73,7 @@ document.getElementById('img').addEventListener('change', archivo, false);
 function getOCR(user) {
     userConnected = user;
     hidePanel();
-    var formData = new FormData($('#upload-img')[0]);
+    var formData = new FormData($('#new')[0]);
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8000/upload',
@@ -91,7 +92,7 @@ function getOCR(user) {
 // Tratamiento de texto
 // ###########################################################
 function pln(txt) {
-    var filterWords = txt.split('-');
+    var filterWords = txt.replace(/\n/g, ' ').split(' ');
     for (var i = filterWords.length - 1; i >= 0; i--) {
         if (filterWords[i].length < 3 || filterWords[i].match(/^upt/) || filterWords[i].match(/^univer/) || filterWords[i].match(/^hote/) || filterWords[i].match(/^compu/) || filterWords[i].match(/^edu/) || filterWords[i].match(/^matema/) || filterWords[i].match(/^elec/) || filterWords[i].match(/^peda/) || filterWords[i].match(/^natural/) || filterWords[i].match(/^enfermer/) || filterWords[i].match(/^profe/) || filterWords[i].match(/^agro/) || filterWords[i].match(/^huma/) || filterWords[i].match(/^estadis/) || filterWords[i].match(/^merca/) || filterWords[i].match(/^psico/) || filterWords[i].match(/^trans/) || filterWords[i].match(/^fisi/) || filterWords[i].match(/^bio/) || filterWords[i].match(/^extra/) || filterWords[i].match(/^pre/) || filterWords[i].match(/^econo/) || filterWords[i].match(/^cien/) || filterWords[i].match(/^zoo/) || filterWords[i].match(/^admin/) || filterWords[i].match(/^indus/) || filterWords[i].match(/^filo/) || filterWords[i].match(/^empre/) || filterWords[i].match(/^ambien/) || filterWords[i].match(/[0-9]/) || filterWords[i].match(/^geo/) || filterWords[i].match(/^finan/) || filterWords[i].match(/^comer/) || filterWords[i].match(/^tecn/) || filterWords[i].match(/^prod/) || filterWords[i].match(/^lic/) || filterWords[i].match(/gica/) || filterWords[i].match(/^aseso/) || filterWords[i].match(/fasis$/) || filterWords[i].match(/^mensa/) || filterWords[i].match(/ypțę/) || filterWords[i].match(/^cod/) || filterWords[i].match(/^sist/) || filterWords[i].match(/tunja/) || filterWords[i].match(/^ing/) || filterWords[i].match(/^www/) || filterWords[i].match(/cembia/) || filterWords[i].match(/^origi/) || filterWords[i].match(/ohmbu/) || filterWords[i].match(/del/) || filterWords[i].match(/acero/) || filterWords[i].match(/coo/) || filterWords[i].match(/vias/) || filterWords[i].match(/^derec/) || filterWords[i].match(/^socia/) || filterWords[i].match(/civil/) || filterWords[i].match(/^lengu/) || filterWords[i].match(/finanzas/) || filterWords[i].match(/^especia/) || filterWords[i].match(/idiomas/) || filterWords[i].match(/modernos/) || filterWords[i].match(/tc/) || filterWords[i].match(/minas/) || filterWords[i].match(/sica$/) || filterWords[i].match(/diseño/) || filterWords[i].match(/sogamoso/) || filterWords[i].match(/^chiquin/) || filterWords[i].match(/t&tc/) || filterWords[i].match(/ñol$/) || filterWords[i].match(/ingles/) || filterWords[i].match(/duitama/) || filterWords[i].match(/medicina/) || filterWords[i].match(/^veteri/) || filterWords[i].match(/procesos/) || filterWords[i].match(/rccnol,igica/) || filterWords[i].match(/gestion/) || filterWords[i].match(/nuevo/) || filterWords[i].match(/chitaraqu/) || filterWords[i].match(/ląc/) || filterWords[i].match(/estudiante/) || filterWords[i].match(/semestre/) || filterWords[i].match(/valido/) || filterWords[i].match(/fcrfaaoos/) || filterWords[i].match(/sionau/) || filterWords[i].match(/ion/) || filterWords[i].match(/musica/) || filterWords[i].match(/atura$/) || filterWords[i].match(/qumca/) || filterWords[i].match(/имс/) || filterWords[i].match(/оамсо/) || filterWords[i].match(/аяманоо/) || filterWords[i].match(/соо/) || filterWords[i].match(/pe=iay/) || filterWords[i].match(/pcd@gógŕ•/) || filterWords[i].match(/^quimi/) || filterWords[i].match(/tuwa/) || filterWords[i].match(/p_țț_,/) || filterWords[i].match(/^colom/) || filterWords[i].match(/^coc/) || filterWords[i].match(/alta/) || filterWords[i].match(/^did/) || filterWords[i].match(/^bases/) || filterWords[i].match(/^servi/) || filterWords[i].match(/^turis/) || filterWords[i].match(/^conta/) || filterWords[i].match(/blica$/) || filterWords[i].match(/^inter/) || filterWords[i].match(/ticas$/) || filterWords[i].match(/ción$/) || filterWords[i].match(/^depor/) || filterWords[i].match(/^recre/) || filterWords[i].match(/nible$/) || filterWords[i].match(/^insta/) || filterWords[i].match(/^rede/) || filterWords[i].match(/^herra/) || filterWords[i].match(/^tele/) || filterWords[i].match(/^farma/) || filterWords[i].match(/ungvers'dad/)) {
             filterWords.splice(i, 1);
@@ -142,7 +143,9 @@ function pipeline(id) {
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8000/pipeline/' + id,
-        data: JSON.stringify({ steps: steps }),
+        data: JSON.stringify({
+            steps: steps
+        }),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
     }).done(function(data) {
@@ -153,23 +156,21 @@ function pipeline(id) {
     });
 }
 
-
 function ocr(id) {
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8000/ocr-individual/' + id,
     }).done(function(data) {
         textDetect = data.results.text;
-        if (textDetect != '') {
-            txtFilter = pln(textDetect);
-            var nameDetect = txtFilter.filterText;
-            for (var index = 0; index < txtFilter.filterText.length; index++) {
-                name = name + " " + txtFilter.filterText[index];
-            }
-            getMembersFacebook(nameDetect);
-        } else {
-            newPost("NN", user);
+        txtFilter = pln(textDetect);
+        var nameDetect = txtFilter.filterText;
+        for (var index = 0; index < txtFilter.filterText.length; index++) {
+            name = name + " " + txtFilter.filterText[index];
         }
+        // getMembersFacebook(nameDetect);
+        console.log(nameDetect);
+        console.log(userConnected);
+        newPost(nameDetect, userConnected);
     }).fail(function(err) {
         console.log(err);
         $.showNotify('Error', 'Ocurrió un error en el procesamiento del texto, intente mas tarde.', 'error');
